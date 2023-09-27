e47b8eceb2d12fec4dbef470b561caeff94b228e
Beksultan Bakytov <beksultanbakytov@MacBook-Air-Beksultan.local>
2023-09-27 23:07:17 +0600

    fixed hero bg

--- src/js/modules/swipers.js
+++ src/js/modules/swipers.js
@@ -2,42 +2,42 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import Swiper from "swiper";
 
 export function commonSwipers() {
-  let swiperHero = new Swiper(".hero-swiper", {
-    speed: 400,
-    spaceBetween: 20,
-    loop: true,
-    slidesPerView: "auto",
-    centeredSlides: true,
-    modules: [Pagination, Autoplay],
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      delay: 4000,
-      pauseOnMouseEnter: true,
-    },
-  });
+    let swiperHero = new Swiper(".hero-swiper", {
+        speed: 400,
+        spaceBetween: 20,
+        loop: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        // modules: [Pagination, Autoplay],
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        // autoplay: {
+        //     delay: 4000,
+        //     pauseOnMouseEnter: true,
+        // },
+    });
 
-  let swiperAdvantages = new Swiper(".advantages-swiper", {
-    speed: 400,
-    spaceBetween: 20,
-    slidesPerView: "auto",
-  });
+    let swiperAdvantages = new Swiper(".advantages-swiper", {
+        speed: 400,
+        spaceBetween: 20,
+        slidesPerView: "auto",
+    });
 
-  let swiperProducts = new Swiper(".products-swiper", {
-    speed: 400,
-    spaceBetween: 24,
-    slidesPerView: "auto",
-    modules: [Navigation],
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-      1277: {
-        slidesPerView: 4,
-      },
-    },
-  });
+    let swiperProducts = new Swiper(".products-swiper", {
+        speed: 400,
+        spaceBetween: 24,
+        slidesPerView: "auto",
+        modules: [Navigation],
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            1277: {
+                slidesPerView: 4,
+            },
+        },
+    });
 }
